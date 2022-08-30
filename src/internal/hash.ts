31f3ffe5d38c00a5be02d8f495bc5b6f02818ffc
Florian Kleber <kleberbaum@erebos.xyz>
2022-08-30 15:16:06 +0000

    feat: add db based authentication
     - add duckdb
     - add sql user query
     - add duckdb s3 connection
     - add django PBKDF2PasswordHasher.verify equvivalent with node crypto

---
+++ src/internal/hash.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto';
+
+export async function verify(password, hash) {
+    return new Promise((resolve, reject) => {
+        try {
+            const keyLength = 32;
+            const arrHash = hash.split('$');
+            const digest = arrHash[0].split('_')[1];
+            const iterations = +arrHash[1];
+            const salt = arrHash[2];
+            const key = arrHash[3];
+
+            crypto.pbkdf2(password, salt, iterations, keyLength, digest, (error, derivedKey) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(derivedKey.toString('base64') === key);
+            });
+        } catch (error) {
+            return reject(error);
+        }
+    });
+};
--- src/internal/storage.ts
+++ src/internal/storage.ts
@@ -1,67 +1,78 @@
-import {Connection, DuckDB, IExecuteOptions, RowResultFormat} from 'node-duckdb'
+import duckdb from "@snek-at/db-duckdb";
 
-const executeOptions: IExecuteOptions = {
-  rowResultFormat: RowResultFormat.Object
-}
+// Fields
+const sqlite_path = process.env.SQLITE_PATH || "db.sqlite3";
+const postgres_url =
+  process.env.POSTGRES_URL ||
+  "postgres://app_user:changeme@localhost:5432/app_db";
+const db_directory = process.env.DB_DIRECTORY || "db_export";
 
-export async function queryDatabaseWithIterator(user: string) {
-  // create new database in memory
-  const db = new DuckDB()
+const s3_region = "eu-central-1";
+const s3_access_key_id = process.env.AWS_ACCESS_KEY_ID || "";
+const s3_secret_access_key = process.env.AWS_SECRET_ACCESS_KEY || "";
+const s3_session_token = process.env.AWS_SESSION_TOKEN || "";
+
+export async function queryDatabaseForUser(username: string) {
+  // create new atabase in memory
+  const con = new duckdb.duckdb.Database(":memory:");
+
   // create a new connection to the database
-  const connection = new Connection(db)
+  if (s3_access_key_id) {
+    con.run("INSTALL httpfs;");
+    con.run("LOAD httpfs;");
 
-  console.log(process.env.AWS_ACCESS_KEY_ID)
-  // perform some queries
-  //console.log(`SET s3_access_key_id='${process.env.AWS_ACCESS_KEY_ID}${process.env.AWS_SECRET_ACCESS_KEY}'`)
-  await connection.executeIterator(`SET s3_region='eu-central-1'`)
-  await connection.executeIterator(
-    `SET s3_access_key_id='${process.env.AWS_ACCESS_KEY_ID}'`
-  )
-  await connection.executeIterator(
-    `SET s3_secret_access_key='${process.env.AWS_SECRET_ACCESS_KEY}'`
-  )
-  if (process.env.AWS_SESSION_TOKEN) {
-    await connection.executeIterator(
-      `SET s3_session_token='${process.env.AWS_SESSION_TOKEN}'`
-    )
+    con.run(`SET s3_region='${s3_region}'`);
+    con.run(`SET s3_access_key_id='${s3_access_key_id}'`);
+    con.run(`SET s3_secret_access_key='${s3_secret_access_key}'`);
+
+    if (process.env.AWS_SESSION_TOKEN) {
+      con.run(`SET s3_session_token='${s3_session_token}'`);
+    }
+
+    con.run(
+      `CREATE TABLE 'user' AS SELECT * FROM parquet_scan('s3://snekauth/_user.parquet');`,
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log(`Successfully imported _user.parquet from s3`);
+      }
+    );
+
+    con.run(
+      `CREATE TABLE 'alias' AS SELECT * FROM parquet_scan('s3://snekauth/_alias.parquet');`,
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log(`Successfully imported _alias.parquet from s3`);
+      }
+    );
   }
 
-  await connection.executeIterator(
-    "CREATE TABLE 'user' AS SELECT * FROM parquet_scan('s3://snekauth/0_user.parquet')"
-  )
-  await connection.executeIterator(
-    "CREATE TABLE 'alias' AS SELECT * FROM parquet_scan('s3://snekauth/1_alias.parquet')"
-  )
-  // await connection.executeIterator("CREATE TABLE 'user' (uid INTEGER, password VARCHAR)")
-  // await connection.executeIterator("CREATE TABLE 'alias' (uid INTEGER, alias VARCHAR)")
-  // await connection.executeIterator("INSERT INTO 'user' VALUES (1 , 'ciscocisco');")
-  // await connection.executeIterator("INSERT INTO 'alias' VALUES (1 , 'cisco');")
-  // await connection.executeIterator("INSERT INTO 'alias' VALUES (1 , 'ci@s.co');")
-  // await connection.executeIterator("EXPORT DATABASE './' (FORMAT PARQUET)")
+  // Perform some queries
 
-  const result = await connection.executeIterator(
-    `SELECT * FROM 'alias' a, 'user' u WHERE a.uid=u.uid AND a.alias='${user}' LIMIT 1`,
-    executeOptions
-  )
+  const promiseConAll = (
+    sql: string,
+    params: duckdb.duckdb.Scalar[]
+  ): Promise<duckdb.duckdb.Row[]> => {
+    return new Promise((resolve, reject) => {
+      con.all(sql, params, (err, res) => {
+        console.log("adsadsadas")
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      });
+    });
+  };
 
-  // const password = await connection.executeIterator(
-  //   "SELECT password FROM 'user' WHERE alias='cisco' LIMIT 1",
-  //   executeOptions,
-  // )
-  //const result = await connection.executeIterator("SELECT id FROM parquet_scan('https://github.com/deepcrawl/node-duckdb/raw/master/src/tests/test-fixtures/alltypes_plain.parquet') WHERE id=5;")
-  //await connection.executeIterator("CREATE TABLE 'user' AS SELECT * FROM 'user.parquet';")
-  //const result = await connection.executeIterator("SELECT * FROM 'user' WHERE id=5;")
+  const rows = await promiseConAll(
+    "SELECT * FROM 'alias' a, 'user' u WHERE a.uid=u.uid AND a.alias=?::STRING LIMIT 1", [username,]
+  );
+  
+  let res = rows[0]
+  console.log(res);
 
-  // fetch and print result
-  const res = result.fetchRow()
-  //console.log(result.fetchRow())
-
-  // release resources
-  connection.close()
-  db.close()
-
-  return res
+  return res;
 }
-// sql = `copy (select * from '${input}') to  '${output}'(FORMAT PARQUET)`;
-
-// db.all(sql, (err,d) => console.log(err ? 'Error: '+err.message : d));
