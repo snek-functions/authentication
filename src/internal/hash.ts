a7facc277a23ad8a6bd9cf8c64ca3cdde214e1d4
schettn <52858351+schettn@users.noreply.github.com>
2022-08-31 16:16:11 +0000

    refactor: cleanup

--- src/internal/hash.ts
+++ src/internal/hash.ts
@@ -1,24 +1,31 @@
-import crypto from 'crypto';
+import { pbkdf2 } from 'crypto'
 
-export async function verify(password, hash) {
-    return new Promise((resolve, reject) => {
-        try {
-            const keyLength = 32;
-            const arrHash = hash.split('$');
-            const digest = arrHash[0].split('_')[1];
-            const iterations = +arrHash[1];
-            const salt = arrHash[2];
-            const key = arrHash[3];
+export async function verify(password: string, hash: string) {
+  return new Promise((resolve, reject) => {
+    try {
+      const keyLength = 32
+      const arrHash = hash.split('$')
+      const digest = arrHash[0].split('_')[1]
+      const iterations = +arrHash[1]
+      const salt = arrHash[2]
+      const key = arrHash[3]
 
-            crypto.pbkdf2(password, salt, iterations, keyLength, digest, (error, derivedKey) => {
-                if (error) {
-                    return reject(error);
-                }
+      pbkdf2(
+        password,
+        salt,
+        iterations,
+        keyLength,
+        digest,
+        (error, derivedKey): void => {
+          if (error) {
+            return reject(error)
+          }
 
-                resolve(derivedKey.toString('base64') === key);
-            });
-        } catch (error) {
-            return reject(error);
+          resolve(derivedKey.toString('base64') === key)
         }
-    });
-};
+      )
+    } catch (error) {
+      return reject(error)
+    }
+  })
+}
--- src/internal/index.ts
+++ src/internal/index.ts
@@ -1,2 +1,2 @@
-export * as storage from './storage.js';
-export * as hash from './hash.js';
+export * as hash from './hash.js'
+export * as storage from './storage.js'
