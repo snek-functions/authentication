e27cb461d1831ca4fc8cdd0732761e5a3a0865a2
Florian Kleber <kleber@snek.at>
2022-10-01 08:17:18 +0100

    fix: import sf deps inside of fn body

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,35 +1,37 @@
-import {fn, spawnChild} from './factory'
-import {hash, storage} from './internal/index.js'
-import {User} from './types'
+import { fn, spawnChild } from './factory'
+import { hash, storage } from './internal/index.js'
+import { User } from './types'
 
 const authenticate = fn<
-  {username: string; password: string},
+  { username: string; password: string },
   {
     user_id: string
   }
 >(
-  async ({username, password}, _, req) => {
+  async ({ username, password }, _, req) => {
     // Does one alias per user really make sense?
+
     // const user = (await storage.queryDatabaseForUser(username)) as User
-    const data = await spawnChild('python', 'internal/storage.py', [username,])
+    const userStr = await spawnChild('venv/bin/python', 'internal/storage.py', [username,])
+    console.log(`test:${userStr}`)
+    const user = JSON.parse(userStr) as User
+    console.log("test: ", user)
+    
+    if (user.user_id != "-1") {
+ 
+      // const user: User = {
+      //   user_id: '1',
+      //   alias: username,
+      //   password: password
+      // }
 
-    console.log(data)
-    // const user: User = {
-    //   user_id: '1',
-    //   alias: username,
-    //   password: password
-    // }
-
-    // const hash_algorithm = hashers.getHasher(hash_name);
-    // hash_algorithm.verify("password", user.password).then(console.log); // prints true
-    // if (await hash.verify(password, user.password_hash)) {
-    //   return {
-    //     user_id: user.user_id.toString()
-    //   }
-    // }
-
-    //throw new Error(`Unable to authenticate: ${username}`)
-    return { user_id: "string;" }
+      if (await hash.verify(password, user.password_hash)) {
+        return {
+          user_id: user.user_id.toString()
+        }
+      }
+    }
+    throw new Error(`Unable to authenticate: ${username}`)
   },
   {
     name: 'authenticate'
