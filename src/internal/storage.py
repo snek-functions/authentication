34bc5ded6fb0cfe3b7fb4ac8f68c3116a6a0f328
Florian Kleber <kleber@snek.at>
2022-10-01 08:25:30 +0100

    chore: improve code quality
     - Remove legacy code
     - Reorder imports

--- src/internal/storage.py
+++ src/internal/storage.py
@@ -1,14 +1,12 @@
-from sys import argv
-import os
 import duckdb
+from sys import argv
 
 env = os.environ.copy()
 
-def main(username):
-    s3_region = env.get("AWS_REGION", "eu-central-1")
-    s3_access_key_id = env.get("AWS_ACCESS_KEY_ID", "off")
-    s3_secret_access_key = env.get("AWS_SECRET_ACCESS_KEY", "off")
-    s3_session_token = env.get("AWS_SESSION_TOKEN", "off")
+if settings.is_offline:
+    con = duckdb.connect(':memory:')
+else:
+    con = duckdb.connect(database=settings.duckdb_path, read_only=False)
 
     #con = duckdb.connect(':memory:')
     con = duckdb.connect(database='/tmp/db.duckdb', read_only=False)
@@ -76,12 +74,48 @@ def main(username):
         ORDER BY user_id \
     ")
 
-    res = con.execute(" \
-        SELECT alias, user_id, password FROM 'alias' a, 'user' u \
-        WHERE a.user_id=u.user_id AND a.alias=?::STRING \
-        LIMIT 1", 
-        (username,)
-    )
+def fetch(username):
+    try:
+        res = con.execute(" \
+            SELECT a.alias, u.user_id, u.password FROM '_alias' a, '_user' u \
+            WHERE a.user_id=u.user_id AND a.alias=?::STRING \
+            LIMIT 1",
+                          (username,)
+                          )
+
+        return res.fetchone()
+
+    except:
+        return ()
+
+
+def main(username):
+    out = f'{{ \
+        "alias": "nobody", \
+        "user_id": "0", \
+        "password_hash": "undefined" \
+    }}'.replace(" ", "")
+
+    fetched = fetch(username)
+
+    if fetched:
+        out = f'{{ \
+            "alias": "{fetched[0]}", \
+            "user_id": "{fetched[1]}", \
+            "password_hash": "{fetched[2]}" \
+        }}'.replace(" ", "")
+    else:
+        pull_db()
+        refetched = fetch(username)
+
+        if refetched:
+            out = f'{{ \
+                "alias": "{refetched[0]}", \
+                "user_id": "{refetched[1]}", \
+                "password_hash": "{refetched[2]}" \
+            }}'.replace(" ", "")
+
+    print(out)
 
     # return user_id
     print(res)
