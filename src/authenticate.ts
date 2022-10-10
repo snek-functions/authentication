5c90c1007e0b26a56031ad0f01b6fe0f6d45a1ff
Florian Kleber <kleberbaum@erebos.xyz>
2022-10-10 18:11:05 +0000

    chore: improve code quality
     - Reorder imports
     - Remove legacy code
     - Remove legacy comments

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -8,26 +8,15 @@ const authenticate = fn<
   }
 >(
   async ({ username, password }, _, req) => {
-    // Does one alias per user really make sense?
-    console.log(process.env.CODESPACE_NAME)
-    // const user = (await storage.queryDatabaseForUser(username)) as User
+    const { hash } = await import('./internal/index.js')
+
     const userStr = await spawnChild('venv/bin/python', 'internal/storage.py', [
       username,
       "false"
     ])
-    console.log(`test:${userStr}`)
     const user = JSON.parse(userStr) as User
-    console.log('test: ', user)
-
-    const { hash } = await import('./internal/index.js')
 
     if (user.user_id != '0') {
-      // const user: User = {
-      //   user_id: '1',
-      //   alias: username,
-      //   password: password
-      // }
-
       if (await hash.verify(password, user.password_hash)) {
         return {
           user_id: user.user_id.toString()
