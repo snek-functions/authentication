a3f83945ea78c9336f3a42f3d15f5bd5e90adfe5
Florian Kleber <kleberbaum@erebos.xyz>
2022-10-13 11:49:59 +0000

    feat: add refetch on unknown user from storage
    Update cache on unknown user.

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -16,26 +16,27 @@ const authenticate = fn<
     ])
     const user = JSON.parse(userStr) as User
 
-    if (user.user_id != '0') {
+    if (user.user_id !== '0') {
       if (await hash.verify(password, user.password_hash)) {
         return {
           user_id: user.user_id.toString()
         }
       }
+    }
 
-      const userStr = await spawnChild('venv/bin/python', 'internal/storage.py', [
-        username,
-        "true"
-      ])
+    const reuserStr = await spawnChild('venv/bin/python', 'internal/storage.py', [
+      username,
+      "true"
+    ])
 
-      const reuser = JSON.parse(userStr) as User
+    const reuser = JSON.parse(reuserStr) as User
 
-      if (await hash.verify(password, reuser.password_hash)) {
-        return {
-          user_id: reuser.user_id.toString()
-        }
+    if (await hash.verify(password, reuser.password_hash)) {
+      return {
+        user_id: reuser.user_id.toString()
       }
     }
+
     throw new Error(`Unable to authenticate: ${username}`)
   },
   {
